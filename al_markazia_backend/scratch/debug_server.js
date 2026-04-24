a6206f28aff822ec298b16a04e7950d0e30ca53c
Alidanoun <Alidanoun@users.noreply.github.com>
2026-04-24 13:25:41 +0300

    chore: remove exposed sensitive files

--- al_markazia_backend/scratch/debug_server.js
+++
@@ -1,9 +0,0 @@
-try {
-  console.log('Starting server load test...');
-  require('../src/server.js');
-  console.log('Server loaded successfully.');
-} catch (error) {
-  console.error('SERVER LOAD ERROR:');
-  console.error(error.stack || error);
-  process.exit(2);
-}
